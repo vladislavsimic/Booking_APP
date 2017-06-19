a28558b76d8a12ca07e5d4fd5a76d77744398f7d
Vladislav Simic <dovlasim@gmail.com>
2017-06-19 19:35:05 +0200

    Image upload ang get finished.

--- BookingApp/BookingApp/Controllers/API/AccommodationsController.cs
+++ BookingApp/BookingApp/Controllers/API/AccommodationsController.cs
@@ -10,6 +10,10 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using BookingApp.Models;
 using BookingApp.Models.AppModel;
+using System.Web;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
 
 namespace BookingApp.Controllers.API
 {
@@ -17,6 +21,8 @@ namespace BookingApp.Controllers.API
     public class AccommodationsController : ApiController
     {
         private BAContext db = new BAContext();
+        public const string ServerUrl = "http://localhost:54042";
+        public const int MaxImageSize = 1024 * 1024 * 1;
 
         [HttpGet]
         [Route("accs", Name = "AccApi")]
@@ -90,6 +96,91 @@ namespace BookingApp.Controllers.API
             db.SaveChanges();
 
             return CreatedAtRoute("AccApi", new { id = accommodation.Id }, accommodation);
+        }
+
+        [HttpGet]
+        [Route("acc/image/{id}")]
+        public string GetImage(int id)
+        {
+            Accommodation accommodation = this.db.Accommodations.FirstOrDefault(x => x.Id == id);
+            if (accommodation.ImageURL == null)
+            {
+                return null;
+            }
+
+            var filePath = accommodation.ImageURL;
+            var fullFilePath = HttpContext.Current.Server.MapPath("~/Content/BookingImages/" + Path.GetFileName(filePath));
+            var relativePath = ServerUrl + "/Content/BookingImages/" + Path.GetFileName(filePath);
+
+            if (File.Exists(fullFilePath))
+            {
+                return relativePath;
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        [Route("image/upload")]
+        [AllowAnonymous]
+        public async Task<HttpResponseMessage> PostUserImage()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+                var fileCount = httpRequest.Files.Count;
+
+                if (fileCount > 0)
+                {
+                    for (int i = 0; i < fileCount; i++)
+                    {   
+                        var postedFile = httpRequest.Files[i];
+                        IList<string> AllowedExtension = new List<string> { ".jpg", ".gif", ".png" };
+                        var fileExtension = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = fileExtension.ToLower();
+
+                        if (!AllowedExtension.Contains(extension))
+                        {
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, "Image extension is not valid.");
+                        }
+                        else if (postedFile.ContentLength > MaxImageSize)
+                        {
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, "Image size is greater than one MB.");
+                        }
+                            var filePath = HttpContext.Current.Server.MapPath("~/Content/BookingImages/" + postedFile.FileName);
+                            postedFile.SaveAs(filePath);
+                            return Request.CreateResponse(HttpStatusCode.Created, "Image successfuly posted.");
+                    }
+                }
+                return Request.CreateResponse(HttpStatusCode.NotFound,"Something wrong.");
+            }
+            catch (Exception)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            /*Dictionary<string, object> dict = new Dictionary<string, object>();
+            try
+            {
+
+                var httpRequest = HttpContext.Current.Request;
+
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+                    }
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, dict);*/
         }
 
         [HttpDelete]
